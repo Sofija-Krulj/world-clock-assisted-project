b3877baea1aed9b2ffa04bd58435bc050d1f22ae
Sofija-Krulj <sofija.krulj@gmail.com>
2025-10-05 14:46:31 +0200

    Adding the time without regard of the Los Angeles time zone

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,10 @@
-let losAngelesElement = document.querySelector(".city-specifications");
+let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
 let losAngelesTime = moment();
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format(
+  "h:mm:ss [<small>]A[</small>]"
+);

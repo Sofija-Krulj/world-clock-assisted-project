b5bdadb51e2f3a297a54a93fa4097590ea8c7ab5
Sofija-Krulj <sofija.krulj@gmail.com>
2025-10-05 15:08:49 +0200

    Including the date and time for paris

--- javascript/index.js
+++ javascript/index.js
@@ -8,3 +8,12 @@ losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format(
   "h:mm:ss [<small>]A[</small>]"
 );
+
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+
+let parisTime = moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");

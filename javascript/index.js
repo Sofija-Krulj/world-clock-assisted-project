f50ef0ebbd75bb08463273252e817d280d9267d0
Sofija-Krulj <sofija.krulj@gmail.com>
2025-10-05 14:28:20 +0200

    Replace dummy date of Los Angels by the correct date

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,6 @@
+let losAngelesElement = document.querySelector(".city-specifications");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment();
 
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
